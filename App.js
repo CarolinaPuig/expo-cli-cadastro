af373cf7deca956dcf2d9344eb5d77c8bda6d824
Carolina Goudromihos Puig <85234562+CarolinaPuig@users.noreply.github.com>
2023-02-14 15:10:02 -0300

    texto do textInput no alert

--- App.js
+++ App.js
@@ -1,64 +1,84 @@
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import React from 'react';
 
-export default function App() {
-  return (
-    <>
-    <View style={styles1.container}>
-      <Text>Sistema de Cadastro</Text>
-    </View>
-    <View style={styles2.container}>
-      <Text numberOfLines={3} selectable={true}>Para conseguir ter acesso a esse sistema você precisa preencher corretamente as informações solicitadas abaixo:</Text>
-    </View>
-    <View styles={styles3.container}>
-      <TextInput
-        placeholder= 'Nome completo'
-        style={{
-        borderColor: 'gray',
-        borderWidth: 1,
-        height: 40,
-        paddingHorizontal: 14
-        }}
-      />
-      <TextInput
-        placeholder= 'E-mail'
-        style={{
-        borderColor: 'gray',
-        borderWidth: 1,
-        height: 40,
-        paddingHorizontal: 14
-        }}
-      />
-      <TextInput
-        onBlur={_=> alert('Seu código de confirmação é 249916')}
-        keyboardType= 'numeric'
-        placeholder= 'Telefone'
-        style={{
-        borderColor: 'gray',
-        borderWidth: 1,
-        height: 40,
-        paddingHorizontal: 14
-        }}
-      />
-      <TextInput
-        keyboardType= 'numeric'
-        placeholder= 'Código de confirmação'
-        style={{
-        borderColor: 'gray',
-        borderWidth: 1,
-        height: 40,
-        paddingHorizontal: 14
-        }}
-      />
-    </View>
-    <View style={{padding:16}}>
-      <Button 
-      color='#6298'
-      onPress={_=> alert('Cadastro realizado com sucesso!')}
-      title={"Cadastre-se"}/>
-    </View>
-  </>
-  );
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ""
+    };
+  }
+
+  cliqueBotao = _ =>{
+    const { nome } = this.state;
+    if (!nome){
+      alert('Informe um nome');
+    }
+    else{
+      alert(`Olá, ${nome}.`);
+    }
+  }
+
+  render(){
+    return (
+      <>
+      <View style={styles1.container}>
+        <Text>Sistema de Cadastro</Text>
+      </View>
+      <View style={styles2.container}>
+        <Text numberOfLines={3} selectable={true}>Para conseguir ter acesso a esse sistema você precisa preencher corretamente as informações solicitadas abaixo:</Text>
+      </View>
+      <View styles={styles3.container}>
+        <TextInput
+          placeholder= 'Nome completo'
+          style={{
+          borderColor: 'gray',
+          borderWidth: 1,
+          height: 40,
+          paddingHorizontal: 14
+          }}
+          onChangeText={nome=>this.setState({ nome })}
+        />
+        <TextInput
+          placeholder= 'E-mail'
+          style={{
+          borderColor: 'gray',
+          borderWidth: 1,
+          height: 40,
+          paddingHorizontal: 14
+          }}
+        />
+        <TextInput
+          onBlur={_=> alert('Seu código de confirmação é 249916')}
+          keyboardType= 'numeric'
+          placeholder= 'Telefone'
+          style={{
+          borderColor: 'gray',
+          borderWidth: 1,
+          height: 40,
+          paddingHorizontal: 14
+          }}
+        />
+        <TextInput
+          keyboardType= 'numeric'
+          placeholder= 'Código de confirmação'
+          style={{
+          borderColor: 'gray',
+          borderWidth: 1,
+          height: 40,
+          paddingHorizontal: 14
+          }}
+        />
+      </View>
+      <View style={{padding:16}}>
+        <Button 
+        color='#6298'
+        onPress={this.cliqueBotao}
+        title={"Cadastre-se"}/>
+      </View>
+    </>
+    );
+  }
 }
 
 const styles1 = StyleSheet.create({
@@ -89,4 +109,3 @@ const styles3 = StyleSheet.create({
   },
 })
 ;
-
